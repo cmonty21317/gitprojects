08c5dd603bdcbd2909f2a2619b3ee28ce6e2c5dd
cmonty21317 <CM67@students.ptcollege.edu>
2024-02-22 11:10:41 -0500

    Update theBigOne.java

--- theBigOne.java
+++ theBigOne.java
@@ -2,10 +2,10 @@ public class theBigOne {
         public static void main(String[] args) {
             System.out.println("Welcome to the big one!");
   
-  benJames
+  /*benJames*/
                        for(int x = 0; x < 1767; x++){
-                        System.out.print(x)
+                        System.out.print(x);
                                 }
-            System.out.println("Welcome to the little one!")
+            System.out.println("Welcome to the little one!");
         }
 }
