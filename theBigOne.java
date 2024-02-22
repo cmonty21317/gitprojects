d5e3c7ffd4e35585805dabf26120654b188b8b9d
b-Jamin-out <145600585+b-Jamin-out@users.noreply.github.com>
2024-02-22 11:01:56 -0500

    What happened in 1767?
    idk

--- theBigOne.java
+++ theBigOne.java
@@ -1,5 +1,8 @@
 public class theBigOne {
         public static void main(String[] args) {
             System.out.println("Welcome to the big one!");
+                        for(int x = 0; x < 1767; x++){
+                        System.out.print(x)
+                                }
         }
 }
